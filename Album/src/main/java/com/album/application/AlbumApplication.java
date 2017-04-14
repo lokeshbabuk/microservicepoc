c0643295a6f4bd7a093eddd66a9eeb6792c06368
Lokesh <lokeshbabuk@rediffmail.com>
2017-04-14 18:10:27 +0530

    Aibum

---
+++ Album/src/main/java/com/album/application/AlbumApplication.java
@@ -0,0 +1,14 @@
+package com.album.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+ 
+@SpringBootApplication
+public class AlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApplication.class, args);
+	}
+}
---
+++ Album/src/main/java/com/album/application/dao/Album.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.album.application.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author M1028349
+ *
+ */
+
+@Entity
+public class Album {
+	
+	@Id
+	private Integer albumid;
+    
+	private String title;
+	private String artistName;
+	
+    public Album() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Album(Integer albumid, String title, String artistName) {
+		super();
+		this.albumid = albumid;
+		this.title = title;
+		this.artistName = artistName;
+	}
+	public Integer getAlbumid() {
+		return albumid;
+	}
+	public void setAlbumid(Integer albumid) {
+		this.albumid = albumid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+
+	
+	
+
+
+}
